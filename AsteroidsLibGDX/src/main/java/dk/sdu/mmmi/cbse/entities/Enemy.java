76891b3540b47e60b1db71df856b525b98264bf6
Tobias Vedsted Sørensen <tobiasvedstedsorensen@gmail.com>
2022-03-17 17:35:43 +0100

    Enemy Added

---
+++ AsteroidsLibGDX/src/main/java/dk/sdu/mmmi/cbse/entities/Enemy.java
@@ -0,0 +1,125 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dk.sdu.mmmi.cbse.entities;
+
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.math.MathUtils;
+import dk.sdu.mmmi.cbse.main.Game;
+import java.util.Random;
+
+/**
+ *
+ * @author Vedsted
+ */
+public class Enemy extends SpaceObject {
+
+    private Random random = new Random();
+    private int behavior;
+
+    private float maxSpeed;
+    private float acceleration;
+    private float deceleration;
+
+    public Enemy() {
+
+        x = Game.WIDTH / 2;
+        y = Game.HEIGHT / 2;
+
+        maxSpeed = 300;
+        acceleration = 200;
+        deceleration = 10;
+
+        shapex = new float[4];
+        shapey = new float[4];
+
+        radians = 3.1415f / 2;
+        rotationSpeed = 3;
+
+    }
+
+    private void setShape() {
+        shapex[0] = x + MathUtils.cos(radians) * 8;
+        shapey[0] = y + MathUtils.sin(radians) * 8;
+
+        shapex[1] = x + MathUtils.cos(radians - 4 * 3.1415f / 5) * 8;
+        shapey[1] = y + MathUtils.sin(radians - 4 * 3.1145f / 5) * 8;
+
+        shapex[2] = x + MathUtils.cos(radians + 3.1415f) * 5;
+        shapey[2] = y + MathUtils.sin(radians + 3.1415f) * 5;
+
+        shapex[3] = x + MathUtils.cos(radians + 4 * 3.1415f / 5) * 8;
+        shapey[3] = y + MathUtils.sin(radians + 4 * 3.1415f / 5) * 8;
+    }
+
+    public void update(float dt) {
+
+        behavior = random.nextInt(5);
+
+        // deceleration
+        float vec = (float) Math.sqrt(dx * dx + dy * dy);
+        if (vec > 0) {
+            dx -= (dx / vec) * deceleration * dt;
+            dy -= (dy / vec) * deceleration * dt;
+        }
+        if (vec > maxSpeed) {
+            dx = (dx / vec) * maxSpeed;
+            dy = (dy / vec) * maxSpeed;
+        }
+
+        // set position
+        x += dx * dt;
+        y += dy * dt;
+
+        // set shape
+        setShape();
+
+        // screen wrap
+        wrap();
+
+        switch (behavior) {
+            // accelerating
+            case 1:
+            case 2:
+
+                dx += MathUtils.cos(radians) * acceleration * dt;
+                dy += MathUtils.sin(radians) * acceleration * dt;
+
+                break;
+
+            case 3:
+                // Turn left            
+                radians += rotationSpeed * dt;
+                break;
+
+            case 4:
+                // Turn right
+                radians -= rotationSpeed * dt;
+                break;
+
+            default:
+            // Drift
+        }
+
+    }
+
+    public void draw(ShapeRenderer sr) {
+
+        sr.setColor(1, 1, 1, 1);
+
+        sr.begin(ShapeRenderer.ShapeType.Line);
+
+        for (int i = 0, j = shapex.length - 1;
+                i < shapex.length;
+                j = i++) {
+
+            sr.line(shapex[i], shapey[i], shapex[j], shapey[j]);
+
+        }
+
+        sr.end();
+
+    }
+
+}
--- AsteroidsLibGDX/src/main/java/dk/sdu/mmmi/cbse/gamestates/PlayState.java
+++ AsteroidsLibGDX/src/main/java/dk/sdu/mmmi/cbse/gamestates/PlayState.java
@@ -1,55 +1,54 @@
 package dk.sdu.mmmi.cbse.gamestates;
 
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import dk.sdu.mmmi.cbse.entities.Enemy;
 import dk.sdu.mmmi.cbse.entities.Player;
 import dk.sdu.mmmi.cbse.managers.GameKeys;
 import dk.sdu.mmmi.cbse.managers.GameStateManager;
 
 public class PlayState extends GameState {
-	
-	private ShapeRenderer sr;
-	
-	private Player player;
-	
-	public PlayState(GameStateManager gsm) {
-		super(gsm);
-	}
-	
-	public void init() {
-		
-		sr = new ShapeRenderer();
-		
-		player = new Player();
-		
-	}
-	
-	public void update(float dt) {
-		
-		handleInput();
-		
-		player.update(dt);
-		
-	}
-	
-	public void draw() {
-		player.draw(sr);
-	}
-	
-	public void handleInput() {
-		player.setLeft(GameKeys.isDown(GameKeys.LEFT));
-		player.setRight(GameKeys.isDown(GameKeys.RIGHT));
-		player.setUp(GameKeys.isDown(GameKeys.UP));
-	}
-	
-	public void dispose() {}
-	
+
+    private ShapeRenderer srPlayer;
+    private ShapeRenderer srEnemy;
+
+    private Player player;
+    private Enemy enemy;
+
+    public PlayState(GameStateManager gsm) {
+        super(gsm);
+    }
+
+    public void init() {
+
+        srPlayer = new ShapeRenderer();
+        srEnemy = new ShapeRenderer();
+
+        player = new Player();
+        enemy = new Enemy();
+
+    }
+
+    public void update(float dt) {
+
+        handleInput();
+
+        player.update(dt);
+        enemy.update(dt);
+
+    }
+
+    public void draw() {
+        player.draw(srPlayer);
+        enemy.draw(srEnemy);
+    }
+
+    public void handleInput() {
+        player.setLeft(GameKeys.isDown(GameKeys.LEFT));
+        player.setRight(GameKeys.isDown(GameKeys.RIGHT));
+        player.setUp(GameKeys.isDown(GameKeys.UP));
+    }
+
+    public void dispose() {
+    }
+
 }
-
-
-
-
-
-
-
-
-
